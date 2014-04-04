23b0f2024cedc404326673fc1072591329d05dab
farrdy <farai.motsi@gmail.com>
2014-04-04 22:27:15 +0200

    test

---
+++ JavascriptPractice/Scripts/ArraysTest.js
@@ -0,0 +1,35 @@
+﻿function Person(firstName,lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+Person.prototype.getFullName = function() {
+
+    return this.firstName + "" + this.lastName;
+};
+
+
+function Employee(firstName,lastName,position) {
+    Person.call(this,firstName,lastName);
+    this.position = position;
+
+
+}
+
+window.alert("asdf");
+window.prompt("sasasa", "saasas");
+if (confirm("Document you want toolbar continue")) {
+    location ="http://www.google.com";
+} else {
+    location.reload(true);
+}
+
+
+Employee.prototype = new Person();
+Employee.prototype.getFullName = function() {
+    var fullName = Person.prototype.getFullName.call(this);
+    return fullName + "," + this.position;
+};
+
+
+var employee = new Employee("farai", "Motsi", "Developer");
+alert(employee.getFullName());
